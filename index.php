d88ed9d513eda386693724f656749240cd55b218
Yerzhan Zhamashev <yerzhanzhamash@gmail.com>
2019-09-22 22:54:19 +0500

    Load speed

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
-      <link rel="stylesheet" href="css/bootstrap-material-design.min.css">
+      <link rel="stylesheet" href="css/bootstrap-material-design.css">
       <script src="js/bootstrap-material-design.js"></script>
     <link
       href="https://fonts.googleapis.com/css?family=Montserrat:500,500i,600,700&display=swap"
