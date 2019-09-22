8905909e20b478d2e96d7f91df57a4c3131332b4
Yerzhan Zhamashev <yerzhanzhamash@gmail.com>
2019-09-22 22:15:45 +0500

    time offset

--- index.php
+++ index.php
@@ -33,13 +33,16 @@
     </div>
     <script>
       // Set the date we're counting down to
-      var countDownDate = new Date("Sep 23, 2019 16:15:00").getTime();
+      var countDownDate = new Date("Sep 23, 2019 16:15:00 GMT+05:00").getTime();
 
       // Update the count down every 1 second
       var x = setInterval(function() {
 
         // Get today's date and time
-        var now = new Date().getTime();
+        var date = new Date()
+        var now = date.getTime();
+        var offset = date.getTimezoneOffset();
+        var now = now + offset;
           
         // Find the distance between now and the count down date
         var distance = countDownDate - now;
