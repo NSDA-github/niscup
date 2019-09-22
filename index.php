fc04da8f59174bb74186a54b0e5fa8ffbe8549a8
Yerzhan Zhamashev <yerzhanzhamash@gmail.com>
2019-09-22 22:05:40 +0500

    timer

--- index.php
+++ index.php
@@ -31,6 +31,38 @@
         <h1 id="dscrptn2">FIFA</h1>
       </div>
     </div>
+    <script>
+      // Set the date we're counting down to
+      var countDownDate = new Date("Sep 23, 2019 16:15:00").getTime();
+
+      // Update the count down every 1 second
+      var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+          
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+          
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+          
+        // Output the result in an element with id="demo"
+        document.getElementById("demo").innerHTML = hours + "h "
+        + minutes + "m " + seconds + "s ";
+          
+        // If the count down is over, write some text 
+        if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+      }, 1000);
+    </script>
+    <h2> ДО НАЧАЛА </h2>
+    <div class="countdown" id="demo"></div>
     <div class="body">
       <h2>МЕСТО ПРОВЕДЕНИЯ</h2>
       <h3>
