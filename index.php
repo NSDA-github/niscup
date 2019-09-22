3b29f39d7fb29fe9adfafb42b133e1492cca36eb
Yerzhan Zhamashev <yerzhanzhamash@gmail.com>
2019-09-22 23:36:28 +0500

    Time fix

--- index.php
+++ index.php
@@ -34,29 +34,29 @@
     <script>
       // Set the date we're counting down to
       var countDownDate = new Date("Sep 23, 2019 16:15:00").getTime();
-
+      var serverDate = <?= time() ?>;
+      var now0 = new Date().getTime();
+      var diff = now0 - serverDate*1000;
       // Update the count down every 1 second
       var x = setInterval(function() {
 
         // Get today's date and time
-        var date = new Date;
-        var date = new Date(date.toUTCString());
-        var now = date.getTime() + (date.getTimezoneOffset() * 60000)
-          
+        var now = new Date().getTime();
+
         // Find the distance between now and the count down date
-        var distance = countDownDate - now - 5*3600000;
-          
+        var distance = countDownDate - now + diff;
+
         // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-          
-        // Output the result in an element with id="demo"
-        document.getElementById("demo").innerHTML = hours + "h "
+
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML = days*24 + hours + "h "
         + minutes + "m " + seconds + "s ";
-          
-        // If the count down is over, write some text 
+
+        // If the count down is finished, write some text
         if (distance < 0) {
           clearInterval(x);
           document.getElementById("demo").innerHTML = "EXPIRED";
