bae40e6782fa1679cda8fc3244e9a110ef9a585a
Yerzhan Zhamashev <yerzhanzhamash@gmail.com>
2019-09-22 20:38:17 +0500

    counter

--- index.php
+++ index.php
@@ -17,9 +17,13 @@
   </head>
   <body>
       <script>
-      function redirect(){
-          window.location = "https://challonge.com/niscupfifa";
-      }</script>
+          var visits = 0;
+            function redirect(){
+                visits++;
+                console.log(visits);
+                window.location = "https://challonge.com/niscupfifa";
+            }
+      </script>
     <div class="outerhead">
       <div class="cell">
         <div class="title"><h1>NIS CUP</h1></div>
@@ -213,6 +217,39 @@
     <div class="foot">
         <p>Made by Novel.Systems</p>
     </div>
+
+    <?php
+    session_start();
+    $counter_name = "counter.txt";
+
+    // Check if a text file exists.
+    // If not create one and initialize it to zero.
+    if (!file_exists($counter_name)) {
+        $f = fopen($counter_name, "w");
+        fwrite($f,"0");
+        fclose($f);
+    }
+
+    // Read the current value of our counter file
+    $f = fopen($counter_name,"r");
+    $counterVal = fread($f, filesize($counter_name));
+    fclose($f);
+
+    // Has visitor been counted in this session?
+    // If not, increase counter value by one
+    if(!isset($_SESSION['hasVisited'])){
+        $_SESSION['hasVisited']="yes";
+        $counterVal++;
+        $f = fopen($counter_name, "w");
+        fwrite($f, $counterVal);
+        fclose($f);
+    }
+    echo "<p>NoV: ";
+    echo $counterVal;
+    echo "</p>"
+    ?>
   </body>
 </html>
+
+
 
